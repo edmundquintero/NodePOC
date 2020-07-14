219d98a532c6d30eacebc97cdab288d3b573c78b
Edmund Quintero <ed.quintero@invest.ally.com>
2020-07-14 18:54:34 -0400

    Cleanup and exports fixed

--- modules/dogs/dogs.controller.js
+++ modules/dogs/dogs.controller.js
@@ -8,6 +8,8 @@ const DOGS = require('../../mocks/dogs.json');
  */
 class DogController {
     static get(req, res){
-        res.status(200).json(DOGS);
+        return res.status(200).json(DOGS);
     }
 }
+
+module.exports = DogController;
--- modules/dogs/dogs.routes.js
+++ modules/dogs/dogs.routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const dogRouter = express.Router();
+const DogController = require('./dogs.controller');
 
 dogRouter.route('/')
     .get(DogController.get);
 
 router.use('/dogs', dogRouter);
 
-module.exports - router;
+module.exports = router;
