0e3a663a503ca5de8de72bad46c350e91cd5729e
Edmund Quintero <ed.quintero@invest.ally.com>
2020-07-15 07:39:26 -0400

    Adds home page and local gallery with css

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 
-const router = require('./server/core/router/router');
+const assets = express.static('./assets');
+app.use(assets);
 
+const router = require('./server/core/router/router');
 app.use(router);
+
 
 app.get('/', function (req, res) {
     res.status(200).send('hello world');
   })
-  
-app.listen(3000);
+
+const host = '127.0.0.1';
+const port = 3000;
+app.listen(port,host,() => console.log(`Server listening on ${host}:${port}`) );
